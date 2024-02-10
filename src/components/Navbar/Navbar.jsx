5378532c198b561233d7de57436613f365ba7314
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-10 20:58:19 +0600

    user list suceessfully in the ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-neutral text-neutral-content">
+            <div className="navbar bg-neutral text-neutral-content w-full my-5">
                 <button className="btn btn-ghost text-xl">Technext Ltd</button>
             </div>
         </div>
--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,12 +1,62 @@
-import Footer from "../Footer/Footer";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/users");
+      setUsers(response.data.users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
   return (
     <div>
       <Navbar></Navbar>
-     <h1>User List Will Show Here</h1>
-     <Footer></Footer>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5 mx-5">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="bg-white rounded-lg shadow-md p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+          >
+            <div className="text-center">
+              <img
+                src={user.image}
+                alt={user.firstName}
+                className="w-24 h-24 rounded-full mx-auto mb-4"
+              />
+
+              <Link to={`/user/${user.id}`}>
+                <p className="font-bold text-lg text-blue-500">
+                  {user.firstName} {user.lastName}
+                </p>
+              </Link>
+              <p className="text-gray-600 text-sm">Email :{user.email}</p>
+              <div className="mt-2 text-sm">
+                <p className="mb-1">
+                  <strong className="text-gray-800">Address:</strong>{" "}
+                  {user.address?.address}, {user.address?.suite},{" "}
+                  {user.address?.city}
+                </p>
+                <p className="mb-1">
+                  <strong className="text-gray-800">Company:</strong>{" "}
+                  {user.company?.name}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
