b11ef3ee6661a41c09e9a8df0908dd4b500845f1
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-10 22:28:59 +0600

    single user deatils show on separate page

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { IoArrowBack } from "react-icons/io5";
+import Navbar from "../Navbar/Navbar";
+
 const UserDetails = () => {
+  const { userId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://dummyjson.com/users/${userId}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user details:", error);
+      });
+  }, [userId]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>User Details Will Show Here</h1>
+      <Navbar></Navbar>
+      <div className="ml-5 mb-10">
+        <Link to="/" className="flex">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
+            <IoArrowBack className="mr-2" /> Go Back
+          </button>
+        </Link>
+      </div>
+      <img
+        src={user.image}
+        alt={user.firstName}
+        className="w-20 h-20 rounded-full mx-auto mb-4"
+      />
+      <div className="text-center">
+        <p className="font-bold">
+          {user.firstName} {user.lastName}
+        </p>
+        <p className="text-gray-600">{user.email}</p>
+        <p className="mt-2">
+          <strong>Address:</strong> {user.address.address}, {user.address.suite}
+          , {user.address.city}
+        </p>
+        <p className="mt-2">
+          <strong>Company:</strong> {user.company.name}
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -3,60 +3,54 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-
 const UserList = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetchUsers();
+    axios
+      .get("https://dummyjson.com/users")
+      .then((response) => {
+        setUsers(response.data.users);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+      });
   }, []);
 
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get("https://dummyjson.com/users");
-      setUsers(response.data.users);
-    } catch (error) {
-      console.error("Error fetching users:", error);
-    }
-  };
   return (
     <div>
       <Navbar></Navbar>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-5 mx-5">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-5 shadow-lg mt-5 ">
         {users.map((user) => (
-          <div
-            key={user.id}
-            className="bg-white rounded-lg shadow-md p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
-          >
-            <div className="text-center">
+          <div key={user.id} className="bg-white rounded-lg shadow-md p-4 mb-5">
+            
+              {" "}
+
               <img
                 src={user.image}
                 alt={user.firstName}
-                className="w-24 h-24 rounded-full mx-auto mb-4"
+                className="w-20 h-20 rounded-full mx-auto mb-4"
               />
-
-              <Link to={`/user/${user.id}`}>
-                <p className="font-bold text-lg text-blue-500">
+              <div className="text-center">
+                <p className="font-bold">
+                <Link to={`/user/${user.id}`}>
                   {user.firstName} {user.lastName}
+                </Link>
                 </p>
-              </Link>
-              <p className="text-gray-600 text-sm">Email :{user.email}</p>
-              <div className="mt-2 text-sm">
-                <p className="mb-1">
-                  <strong className="text-gray-800">Address:</strong>{" "}
-                  {user.address?.address}, {user.address?.suite},{" "}
-                  {user.address?.city}
+                <p className="text-gray-600">{user.email}</p>
+                <p className="mt-2">
+                  <strong>Address:</strong> {user.address.address},{" "}
+                  {user.address.suite}, {user.address.city}
                 </p>
-                <p className="mb-1">
-                  <strong className="text-gray-800">Company:</strong>{" "}
-                  {user.company?.name}
+                <p className="mt-2">
+                  <strong>Company:</strong> {user.company.name}
                 </p>
               </div>
-            </div>
+           
           </div>
         ))}
       </div>
-      <Footer></Footer>
+     <Footer></Footer>
     </div>
   );
 };
